46d71b07cadd586c7c45d8aec66076cb6b69e5de
shintu0 <imailtiwari@gmail.com>
2023-04-29 15:38:43 +0530

    http

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-import fs from "fs";
-import path, { resolve } from "path";
-
 try {
-  const __dirname = path.dirname(new URL(import.meta.url).pathname);
-  console.log(path.dirname(import.meta.url));
-  console.log(process.cwd());
-  console.log(__dirname);
-  await new Promise((res, rej) => {
-    fs.mkdir("output", { recursive: true }, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        resolve("Created");
-      }
-    });
-  });
-  fs.writeFileSync(
-    path.join(process.cwd(), "/output/hello.txt"),
-    "Hello World"
-  );
+  console.log(">>>>>>>START>>>>>");
+  const timer=setTimeout(() => {
+    console.log(">>>>>>>INSIDE>>>>>");
+  }, 3000);
+  console.log(timer);
+  console.log(">>>>>>>END>>>>>");
 } catch (error) {
-  console.log("--------------ERROR-----------------");
-  console.log(error.message);
-} finally {
-  console.log("Done........,_/");
+  console.log("ERROR:" + error.message);
 }
