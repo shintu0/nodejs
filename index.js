32c5e07b24ac32d71efbc3bbae71255f645cd024
shintu0 <imailtiwari@gmail.com>
2023-05-02 11:10:13 +0530

    Basic express setup

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-import  express from "express";
+/**Important import */
+import express from "express";
 import * as dotenv from "dotenv";
 
 try {
-  const app = express();
-  dotenv.config();
+  const app = express(); //Initialized express app
+  dotenv.config(); //configuring env variables
+
+  /**
+   * Endpoint:http://localhost:7000 (Root)
+   * Access: Public
+   * METHOD: GET
+   */
   app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello" });
   });
 
+  /**
+   * Started server on Specific ports
+   */
   app.listen(process.env.PORT, () => {
-    console.log(`Listening on ${process.env.PORT}: http://localhost:${process.env.PORT}/`);
+    console.log(
+      `Listening on ${process.env.PORT}: http://localhost:${process.env.PORT}/`
+    );
   });
 } catch (error) {
+  /**
+   * Caught Error, if any while starting server
+   */
   console.log("Unable to start Server, Error:" + error.message);
 }
