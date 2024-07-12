641ccbb4b570a364d675885716fdafce33994d86
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-07-12 06:38:39 +0000

    event loop and clusters

---
+++ src/core/threads/childeProcess.js
@@ -0,0 +1,9 @@
+import { exec } from 'child_process';
+exec('ls -lh /usr', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
