f0a77373b2797f9d7b753da6fd630066bf0e130b
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-08-22 05:16:01 +0000

    object

--- __test__/api.spec.js
+++ __test__/api.spec.js
@@ -1,11 +1,15 @@
 import supertest from 'supertest';
 import { app } from "../index.js";
 
+import request from 'supertest';
+
+
 
 describe("API", () => {
   test("API Success", async() => {
     const response =await supertest(app).get('/');
     console.log(response.text)
     expect(response).not.toBeNull();
+    // expect(response).toBeDefined();
   });
 });
