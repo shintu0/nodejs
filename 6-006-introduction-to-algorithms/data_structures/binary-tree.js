f0b69e5b2b31cf5540738711d6f1f62abfc65207
Sintu Tiwari <125259725+shintu0@users.noreply.github.com>
2025-03-02 06:05:26 +0000

    Lectures

---
+++ 6-006-introduction-to-algorithms/data_structures/binary-tree.js
@@ -0,0 +1,24 @@
+class BinaryNode{
+    constructor(parent,left,right,item){
+        this.parent=parent;
+        this.left=left;
+        this.right=right;
+        this.item=item;
+    }
+}
+
+class BinaryTree{
+    constructor(item){
+        this.root=new BinaryNode(null,null,null,item);
+    }
+
+    add(item){
+
+    }
+    find(item){
+
+    }
+    traverse(){
+
+    }
+}
