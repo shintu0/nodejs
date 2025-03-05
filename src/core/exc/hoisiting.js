ab81edca973fdb4a47e692442c601de7e96c10e5
Sintu Tiwari <125259725+shintu0@users.noreply.github.com>
2025-03-05 16:33:25 +0000

    Hoisitng

--- src/core/exc/hoisiting.js
+++ src/core/exc/hoisiting.js
@@ -16,3 +16,40 @@ works in browser
 d=10;
 let d=30;
 */
+
+let obj1={
+    a:10
+}
+let obj2=obj1;
+console.log(obj1==obj2)
+console.log(obj1===obj2)
+obj2=Object.create(obj2);
+
+console.log(obj1==obj2)
+console.log(obj1===obj2)
+
+function mulInf(a){
+    return (b)=>(c)=>a*b*c;
+}
+
+console.log(mulInf(1)(2)(3));
+
+console.log(greet)
+// console.log(greet1)// error
+// console.log(greet3)// error
+
+function greet(){
+    console.log("1");
+}
+
+const greet1=function (){
+    console.log("2");
+};
+
+const greet3=()=>{
+    console.log("3")
+}
+
+console.log(greet)
+console.log(greet1)
+console.log(greet3)
