4fe73aab1b4fa1100ae06b0c8230dba2af37f2fb
Sintu Tiwari <125259725+shintu0@users.noreply.github.com>
2025-03-09 10:34:01 +0000

    SQL practice

---
+++ db/practice-problem.sql
@@ -0,0 +1,29 @@
+/*
+
+Basic SQL Questions
+Select all employees from the Sales department.
+
+Find the total number of employees in each department.
+
+Get the names of employees who were hired after 2020.
+
+Find the average salary of employees.
+
+List all employees sorted by their salary in descending order.
+
+Intermediate SQL Questions
+Find the total sales amount for each employee.
+
+Find employees who have not placed any orders.
+
+Find the department with the highest total salary.
+
+Find the employee with the highest single order amount.
+
+Find the total number of orders placed by each department.
+
+delete duplicate records
+
+Advanced SQL Question (Bonus)
+Find the employees who earn more than the average salary of their department.
+*/
